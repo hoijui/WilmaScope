15e91df59b68674cd318e9e98cbc1ac272e1c1d8
Tim Dwyer <tgdwyer@gmail.com>
2001-09-03 02:35:15 +0000

    blah

--- Wilma/src/org/wilmascope/gui/HtmlPane.java
+++ Wilma/src/org/wilmascope/gui/HtmlPane.java
@@ -1,12 +1,34 @@
 package org.wilmascope.gui;
 
 /**
- * Title:
- * Description:
- * Copyright:    Copyright (c) 2001
- * Company:
- * @author
- * @version 1.0
+ * The following came from one of the Sun examples... I can't recall which one but it's been butchered pretty badly since then, still you should probably read the standard sun disclaimers before you try to use it in your own project:
+ *
+ * Copyright (c) 1998, 1999 by Sun Microsystems, Inc. All Rights Reserved.
+ * 
+ * Sun grants you ("Licensee") a non-exclusive, royalty free, license to use,
+ * modify and redistribute this software in source and binary code form,
+ * provided that i) this copyright notice and license appear on all copies of
+ * the software; and ii) Licensee does not utilize the software in a manner
+ * which is disparaging to Sun.
+ * 
+ * This software is provided "AS IS," without a warranty of any kind. ALL
+ * EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND WARRANTIES, INCLUDING ANY
+ * IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR
+ * NON-INFRINGEMENT, ARE HEREBY EXCLUDED. SUN AND ITS LICENSORS SHALL NOT BE
+ * LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING
+ * OR DISTRIBUTING THE SOFTWARE OR ITS DERIVATIVES. IN NO EVENT WILL SUN OR ITS
+ * LICENSORS BE LIABLE FOR ANY LOST REVENUE, PROFIT OR DATA, OR FOR DIRECT,
+ * INDIRECT, SPECIAL, CONSEQUENTIAL, INCIDENTAL OR PUNITIVE DAMAGES, HOWEVER
+ * CAUSED AND REGARDLESS OF THE THEORY OF LIABILITY, ARISING OUT OF THE USE OF
+ * OR INABILITY TO USE SOFTWARE, EVEN IF SUN HAS BEEN ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGES.
+ * 
+ * This software is not designed or intended for use in on-line control of
+ * aircraft, air traffic, aircraft navigation or aircraft communications; or in
+ * the design, construction, operation or maintenance of any nuclear
+ * facility. Licensee represents and warrants that it will not use or
+ * redistribute the Software for such purposes.
+
  */
 import javax.swing.*;
 import java.awt.*;
@@ -20,33 +42,53 @@ import java.awt.event.*;
 public class HtmlPane extends JScrollPane implements HyperlinkListener {
     JEditorPane html;
 
-    public HtmlPane(String helpFileName) {
-	try {
-	    File f = new File (helpFileName);
-	    String s = f.getAbsolutePath();
-	    s = "file:"+s;
-	    URL url = new URL(s);
-	    html = new JEditorPane(s);
-	    html.setEditable(false);
-	    html.addHyperlinkListener(this);
 
-	    JViewport vp = getViewport();
-	    vp.add(html);
-	} catch (MalformedURLException e) {
-	    System.out.println("Malformed URL: " + e);
-	} catch (IOException e) {
-	    System.out.println("IOException: " + e);
-	}	
+    public HtmlPane(String helpFileName) {
+        try {
+            File f = new File (helpFileName);
+            String s = f.getAbsolutePath();
+            s = "file:"+s;
+            URL url = new URL(s);
+            html = new JEditorPane(s);
+            html.setEditable(false);
+            html.addHyperlinkListener(this);
+
+            JViewport vp = getViewport();
+            vp.add(html);
+        } catch (MalformedURLException e) {
+            System.out.println("Malformed URL: " + e);
+        } catch (IOException e) {
+            System.out.println("IOException: " + e);
+        }       
     }
+    public void setText(String text) {
+        html.setText(text);
+    }
+ public void init() {
+    html.addHyperlinkListener(this);
+    JViewport vp = getViewport();
+    html.setSize(400,400);
+    vp.add(html);
+    vp.updateUI();
+  }
+
+
+ public HtmlPane(String type, String text) {
+    html = new JEditorPane(type, text);
+    html.addHyperlinkListener(this);
+    html.setEditable(false);
+    init();
+  }
+
 
     /**
      * Notification of a change relative to a 
      * hyperlink.
      */
     public void hyperlinkUpdate(HyperlinkEvent e) {
-	if (e.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
-	    linkActivated(e.getURL());
-	}
+        if (e.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
+            linkActivated(e.getURL());
+        }
     }
 
     /**
@@ -62,10 +104,10 @@ public class HtmlPane extends JScrollPane implements HyperlinkListener {
      * @param u the URL to follow
      */
     protected void linkActivated(URL u) {
-	Cursor c = html.getCursor();
-	Cursor waitCursor = Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR);
-	html.setCursor(waitCursor);
-	SwingUtilities.invokeLater(new PageLoader(u, c));
+        Cursor c = html.getCursor();
+        Cursor waitCursor = Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR);
+        html.setCursor(waitCursor);
+        SwingUtilities.invokeLater(new PageLoader(u, c));
     }
 
     /**
@@ -74,39 +116,39 @@ public class HtmlPane extends JScrollPane implements HyperlinkListener {
      * can be done).
      */
     class PageLoader implements Runnable {
-	
-	PageLoader(URL u, Cursor c) {
-	    url = u;
-	    cursor = c;
-	}
+        
+        PageLoader(URL u, Cursor c) {
+            url = u;
+            cursor = c;
+        }
 
         public void run() {
-	    if (url == null) {
-		// restore the original cursor
-		html.setCursor(cursor);
+            if (url == null) {
+                // restore the original cursor
+                html.setCursor(cursor);
 
-		// PENDING(prinz) remove this hack when 
-		// automatic validation is activated.
-		Container parent = html.getParent();
-		parent.repaint();
-	    } else {
-		Document doc = html.getDocument();
-		try {
-		    html.setPage(url);
-		} catch (IOException ioe) {
-		    html.setDocument(doc);
-		    getToolkit().beep();
-		} finally {
-		    // schedule the cursor to revert after
-		    // the paint has happended.
-		    url = null;
-		    SwingUtilities.invokeLater(this);
-		}
-	    }
-	}
+                // PENDING(prinz) remove this hack when 
+                // automatic validation is activated.
+                Container parent = html.getParent();
+                parent.repaint();
+            } else {
+                Document doc = html.getDocument();
+                try {
+                    html.setPage(url);
+                } catch (IOException ioe) {
+                    html.setDocument(doc);
+                    getToolkit().beep();
+                } finally {
+                    // schedule the cursor to revert after
+                    // the paint has happended.
+                    url = null;
+                    SwingUtilities.invokeLater(this);
+                }
+            }
+        }
 
-	URL url;
-	Cursor cursor;
+        URL url;
+        Cursor cursor;
     }
 
 }
