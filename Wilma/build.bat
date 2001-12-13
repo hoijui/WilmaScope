aaf572f023fa235c74eeb37e5b9c1a022853737a
Tim Dwyer <tgdwyer@gmail.com>
2001-12-13 00:47:20 +0000

    Trying to make the windows build script more robust

--- Wilma/build.bat
+++ Wilma/build.bat
@@ -1,6 +1,8 @@
 @echo off
 if %JAVA_HOME%a==a goto JAVA_HOME_NOTSET_ERROR
-java -cp %JAVA_HOME%\lib\tools.jar;lib\ant.jar;lib\jaxp.jar;lib\crimson.jar;lib\xalan.jar org.apache.tools.ant.Main %1
+echo If you get an error message on the next line then you've probably set your JAVA_HOME variable incorrectly:
+%JAVA_HOME%\bin\java -version
+%JAVA_HOME%\bin\java -cp %JAVA_HOME%\lib\tools.jar;lib\ant.jar;lib\jaxp.jar;lib\crimson.jar;lib\xalan.jar org.apache.tools.ant.Main %1
 goto DONE
 :JAVA_HOME_NOTSET_ERROR
 echo You must set the JAVA_HOME variable, eg: set JAVA_HOME=c:\jdk1.3.1
