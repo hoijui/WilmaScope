a79ee5280bcdbd9dfc97fb5f04f54800161c818e
Tim Dwyer <tgdwyer@gmail.com>
2002-01-24 07:37:41 +0000

    new class for specifying constraints on nodes within the force layout system

---
+++ src/org/wilmascope/forcelayout/Constraint.java
@@ -0,0 +1,14 @@
+package org.wilmascope.forcelayout;
+
+/**
+ * <p>Title: </p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2001</p>
+ * <p>Company: </p>
+ * @author unascribed
+ * @version 1.0
+ */
+
+public abstract class Constraint {
+  public abstract void apply(NodeForceLayout n);
+}
