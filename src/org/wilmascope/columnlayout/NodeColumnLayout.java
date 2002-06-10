425e4819c8068d716e3db4d6a4b4884f89e59b3b
Tim Dwyer <tgdwyer@gmail.com>
2002-06-10 04:18:02 +0000

    3D column graph layout

---
+++ src/org/wilmascope/columnlayout/NodeColumnLayout.java
@@ -0,0 +1,32 @@
+package org.wilmascope.columnlayout;
+
+import org.wilmascope.forcelayout.NodeForceLayout;
+import javax.vecmath.Vector3f;
+
+/**
+ * <p>Title: </p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2001</p>
+ * <p>Company: </p>
+ * @author unascribed
+ * @version 1.0
+ */
+
+public class NodeColumnLayout extends NodeForceLayout {
+    public NodeColumnLayout(int level) {
+        setLevel(level);
+    }
+    public void setLevel(int level) {
+        this.level = level;
+    }
+    public int getLevel() {
+        return level;
+    }
+    public void addForce(Vector3f f) {
+        ((NodeForceLayout)getNode().getOwner().getLayout()).addForce(f);
+    }
+    public void subForce(Vector3f f) {
+        ((NodeForceLayout)getNode().getOwner().getLayout()).subForce(f);
+    }
+    int level;
+}
