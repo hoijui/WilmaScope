61e010e2f4aefa58f661d49538cbcd3120a6b392
Tim Dwyer <tgdwyer@gmail.com>
2004-06-28 07:49:23 +0000

    default colour gave everything a bluish tinge.  Fixed.

--- Wilma/src/org/wilmascope/viewplugin/TaperedEdgeView.java
+++ Wilma/src/org/wilmascope/viewplugin/TaperedEdgeView.java
@@ -17,9 +17,7 @@
  *
  * -- Tim Dwyer, 2001
  */
-
 package org.wilmascope.viewplugin;
-
 import javax.media.j3d.GeometryStripArray;
 import javax.media.j3d.Material;
 import javax.media.j3d.Shape3D;
@@ -28,135 +26,128 @@ import javax.vecmath.Color3f;
 import javax.vecmath.Point3f;
 import javax.vecmath.Vector3d;
 import javax.vecmath.Vector3f;
-
 import org.wilmascope.graph.Edge;
 import org.wilmascope.view.Colours;
 import org.wilmascope.view.Constants;
 import org.wilmascope.view.EdgeView;
 import org.wilmascope.view.NodeView;
-
 import com.sun.j3d.utils.geometry.Cylinder;
 import com.sun.j3d.utils.geometry.GeometryInfo;
 import com.sun.j3d.utils.geometry.NormalGenerator;
-
 /**
  * Graphical representation of the edge
- *
+ * 
  * @author Tim Dwyer
  * @version 1.0
  */
-
 public class TaperedEdgeView extends EdgeView {
-  //
-  // create the basic reference geometries from the shape sections of a cylinder
-  //
-  static float topRadius = 1f, bottomRadius = 1f;
-  static Point3f[] tubePoints;
-  static int[] tubeStripCounts;
-  static NormalGenerator normalGenerator = new NormalGenerator();
-  {
-    Cylinder c = new Cylinder(1f, 1f);
-    GeometryStripArray tubeGeometry = getGeometry(c, Cylinder.BODY);
-    GeometryStripArray topGeometry = getGeometry(c, Cylinder.TOP);
-    GeometryStripArray bottomGeometry = getGeometry(c, Cylinder.BOTTOM);
-    tubePoints = new Point3f[tubeGeometry.getVertexCount()];
-    tubeStripCounts = new int[tubeGeometry.getNumStrips()];
-    loadGeometry(tubeGeometry, tubeStripCounts, tubePoints);
-  }
-  private static GeometryStripArray getGeometry(Cylinder c, int section) {
-    return (GeometryStripArray) c.getShape(section).getGeometry();
-  }
-  private static void loadGeometry(
-    GeometryStripArray geometry,
-    int[] stripCounts,
-    Point3f[] points) {
-    for (int i = 0; i < points.length; i++) {
-      points[i] = new Point3f();
-    }
-    geometry.getCoordinates(0, points);
-    geometry.getStripVertexCounts(stripCounts);
-  }
-
-  float length = 1.0f;
-  public TaperedEdgeView() {
-    setTypeName("Tapered Edge");
-  }
-  protected void setupDefaultMaterial() {
-    Material material = new Material();
-    material.setDiffuseColor(0.0f, 0.0f, 1.0f);
-    material.setAmbientColor(0f, 0f, 0.4f);
-    material.setShininess(50.0f);
-    setupDefaultAppearance(material);
-  }
-  protected void setupHighlightMaterial() {
-    setupHighlightAppearance(Colours.yellowMaterial);
-  } /**
-   * adjust the radius of the top of the tube
-   * @parameter r xScale factor, ie the resulting top radius will be r * node radius
-   */
-  static public void setTopRadius(float r) {
-    topRadius = r;
-  }
-  /**
-   * adjust the radius of the bottom of the tube
-   * @parameter r xScale factor, ie the resulting bottom radius will be r * node radius
-   */
-  static public void setBottomRadius(float r) {
-    bottomRadius = r;
-  }
-  public void init() {
-    Point3f[] taperedTubePoints = new Point3f[tubePoints.length];
-    Color3f[] tubeColours = new Color3f[tubePoints.length];
-    Color3f startColour = new Color3f(((NodeView)getEdge().getStart().getView()).getColour());
-    Color3f endColour = new Color3f(((NodeView)getEdge().getEnd().getView()).getColour());
-    setBottomRadius(getEdge().getStart().getRadius()*9.5f);
-    setTopRadius(getEdge().getEnd().getRadius()*9.5f);
-    
-    setRadius(0.1f);
-    for (int i = 0; i < tubePoints.length; i++) {
-      if (i % 2 == 0) {
-        taperedTubePoints[i] =
-          new Point3f(
-            tubePoints[i].x * topRadius,
-            tubePoints[i].y,
-            tubePoints[i].z * topRadius);
-        tubeColours[i] = endColour;
-      } else {
-        taperedTubePoints[i] =
-          new Point3f(
-            tubePoints[i].x * bottomRadius,
-            tubePoints[i].y,
-            tubePoints[i].z * bottomRadius);
-        tubeColours[i] = startColour;
-      }
-    }
-    GeometryInfo gi = new GeometryInfo(GeometryInfo.TRIANGLE_STRIP_ARRAY);
-    gi.setCoordinates(taperedTubePoints);
-    gi.setColors(tubeColours);
-    gi.setStripCounts(tubeStripCounts);
-    normalGenerator.generateNormals(gi);
-    Shape3D tubeShape = new Shape3D(gi.getGeometryArray(), getAppearance());
-    makePickable(tubeShape);
-    addTransformGroupChild(tubeShape);
-  }
-  /**
-   * draw the edge correctly between the start and end nodes
-   */
-  public void draw() {
-    Edge e = getEdge();
-    e.recalculate();
-    double l = e.getLength();
-    // avoids non-affine transformations, by making sure edge always has
-    // non-zero length
-    if (e.getLength() == 0) {
-      e.setVector(Constants.gc.getVector3f("MinVector"));
-      l = e.getVector().length();
-    }
-    Vector3f v = new Vector3f(e.getVector());
-    v.scaleAdd(0.5f, e.getStart().getPosition());
-    setFullTransform(new Vector3d(getRadius(), l, getRadius()), v, getPositionAngle());
-  }
-  public ImageIcon getIcon() {
-    return new ImageIcon("images/taperedEdge.png");
-  }
+	//
+	// create the basic reference geometries from the shape sections of a
+	// cylinder
+	//
+	static float topRadius = 1f, bottomRadius = 1f;
+	static Point3f[] tubePoints;
+	static int[] tubeStripCounts;
+	static NormalGenerator normalGenerator = new NormalGenerator();
+	{
+		Cylinder c = new Cylinder(1f, 1f);
+		GeometryStripArray tubeGeometry = getGeometry(c, Cylinder.BODY);
+		GeometryStripArray topGeometry = getGeometry(c, Cylinder.TOP);
+		GeometryStripArray bottomGeometry = getGeometry(c, Cylinder.BOTTOM);
+		tubePoints = new Point3f[tubeGeometry.getVertexCount()];
+		tubeStripCounts = new int[tubeGeometry.getNumStrips()];
+		loadGeometry(tubeGeometry, tubeStripCounts, tubePoints);
+	}
+	private static GeometryStripArray getGeometry(Cylinder c, int section) {
+		return (GeometryStripArray) c.getShape(section).getGeometry();
+	}
+	private static void loadGeometry(GeometryStripArray geometry,
+			int[] stripCounts, Point3f[] points) {
+		for (int i = 0; i < points.length; i++) {
+			points[i] = new Point3f();
+		}
+		geometry.getCoordinates(0, points);
+		geometry.getStripVertexCounts(stripCounts);
+	}
+	float length = 1.0f;
+	public TaperedEdgeView() {
+		setTypeName("Tapered Edge");
+	}
+	protected void setupDefaultMaterial() {
+		Color3f aColor = new Color3f(0.1f, 0.1f, 0.1f);
+		Color3f eColor = new Color3f(0.0f, 0.0f, 0.0f);
+		Color3f dColor = new Color3f(0.6f, 0.6f, 0.6f);
+		Color3f sColor = new Color3f(1.0f, 1.0f, 1.0f);
+		Material m = new Material(aColor, eColor, dColor, sColor, 100.0f);
+		m.setLightingEnable(true);
+		setupDefaultAppearance(m);
+	}
+	protected void setupHighlightMaterial() {
+		setupHighlightAppearance(Colours.yellowMaterial);
+	}
+	/**
+	 * adjust the radius of the top of the tube @parameter r xScale factor, ie
+	 * the resulting top radius will be r * node radius
+	 */
+	static public void setTopRadius(float r) {
+		topRadius = r;
+	}
+	/**
+	 * adjust the radius of the bottom of the tube @parameter r xScale factor, ie
+	 * the resulting bottom radius will be r * node radius
+	 */
+	static public void setBottomRadius(float r) {
+		bottomRadius = r;
+	}
+	public void init() {
+		Point3f[] taperedTubePoints = new Point3f[tubePoints.length];
+		Color3f[] tubeColours = new Color3f[tubePoints.length];
+		Color3f startColour = new Color3f(((NodeView) getEdge().getStart()
+				.getView()).getColour());
+		Color3f endColour = new Color3f(((NodeView) getEdge().getEnd().getView())
+				.getColour());
+		setBottomRadius(getEdge().getStart().getRadius() * 9.5f);
+		setTopRadius(getEdge().getEnd().getRadius() * 9.5f);
+		setRadius(0.1f);
+		for (int i = 0; i < tubePoints.length; i++) {
+			if (i % 2 == 0) {
+				taperedTubePoints[i] = new Point3f(tubePoints[i].x * topRadius,
+						tubePoints[i].y, tubePoints[i].z * topRadius);
+				tubeColours[i] = endColour;
+			} else {
+				taperedTubePoints[i] = new Point3f(tubePoints[i].x * bottomRadius,
+						tubePoints[i].y, tubePoints[i].z * bottomRadius);
+				tubeColours[i] = startColour;
+			}
+		}
+		GeometryInfo gi = new GeometryInfo(GeometryInfo.TRIANGLE_STRIP_ARRAY);
+		gi.setCoordinates(taperedTubePoints);
+		gi.setColors(tubeColours);
+		gi.setStripCounts(tubeStripCounts);
+		normalGenerator.generateNormals(gi);
+		Shape3D tubeShape = new Shape3D(gi.getGeometryArray(), getAppearance());
+		makePickable(tubeShape);
+		addTransformGroupChild(tubeShape);
+	}
+	/**
+	 * draw the edge correctly between the start and end nodes
+	 */
+	public void draw() {
+		Edge e = getEdge();
+		e.recalculate();
+		double l = e.getLength();
+		// avoids non-affine transformations, by making sure edge always has
+		// non-zero length
+		if (e.getLength() == 0) {
+			e.setVector(Constants.gc.getVector3f("MinVector"));
+			l = e.getVector().length();
+		}
+		Vector3f v = new Vector3f(e.getVector());
+		v.scaleAdd(0.5f, e.getStart().getPosition());
+		setFullTransform(new Vector3d(getRadius(), l, getRadius()), v,
+				getPositionAngle());
+	}
+	public ImageIcon getIcon() {
+		return new ImageIcon("images/taperedEdge.png");
+	}
 }
