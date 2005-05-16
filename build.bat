ed4d7a8e6ed9833807b1fe0c5f60816f2cf56ed8
Tim Dwyer <tgdwyer@gmail.com>
2005-05-16 03:16:39 +0000

    Updated for newer version of ant

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 if "%JAVA_HOME%"a==a goto JAVA_HOME_NOTSET_ERROR
 echo If you get an error message on the next line then you've probably set your JAVA_HOME variable incorrectly:
 "%JAVA_HOME%"\bin\java -version
-"%JAVA_HOME%"\bin\java -cp "%JAVA_HOME%"\lib\tools.jar;lib\ant.jar;lib\jaxp.jar;lib\crimson.jar;lib\xalan.jar org.apache.tools.ant.Main %1
+"%JAVA_HOME%"\bin\java -cp "%JAVA_HOME%"\lib\tools.jar;lib\ant.jar;lib\ant-launcher.jar org.apache.tools.ant.Main %1
 goto DONE
 :JAVA_HOME_NOTSET_ERROR
 echo You must set the JAVA_HOME variable, eg: set JAVA_HOME=c:\jdk1.3.1
